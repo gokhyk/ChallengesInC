36c21608d9832bd52148fc2baac5da5fd5290460
George Kula <gokhankula@yahoo.com>
2025-10-21 18:47:23 -0500

    Step Two In this step your goal is to support the command line option -l that outputs the number of lines in a file.

--- ccwc/ccwc.c
+++ ccwc/ccwc.c
@@ -5,13 +5,53 @@ In this step your goal is to write a simple version of wc, let’s call it ccwc
 the number of bytes in a file.
 */
 
+/*
+Step Two
+In this step your goal is to support the command line option -l that outputs the number of lines in a file.
+If you’ve done it right your output should match this:
+>ccwc -l test.txt
+    7145 test.txt
+*/
+
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    long bytes;
+    long lines;
+} Count;
+
+typedef struct {
+    int bytes;
+    int lines;
+} Flag;
+
 void print_usage() {
-    printf("Usage: ccwc -c <filename>\n");
+    printf("Usage: ccwc -c -l <filename>\n");
     printf("Counts the number of bytes in the specified file.\n");
 }
+
+void count_file(FILE *fp, Count *count) {
+    count->bytes = 0;
+    count->lines = 0;
+
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        
+        count->bytes++;
+        if (ch == '\n') {
+            count->lines++;
+        }
+    }
+}
+
+void print_count(Count count, Flag flag) {
+    if (flag.bytes == 1)
+        printf("%8ld", count.bytes);
+    if (flag.lines == 1)
+        printf("%8ld", count.lines);
+} 
 
 int main(int argc, char *argv[]) {
 
@@ -24,14 +64,16 @@ int main(int argc, char *argv[]) {
     }
 
     int i = 1;
-    int byte_count_flag = 0;
-    int byte_count = 0;
+    Flag flag = {0,0};
+    Count count = {0,0};
 
     while (i < argc) {
 
         if (argv[i] != NULL && argv[i][0] == '-') {
             if (strcmp(argv[i], "-c") == 0)
-                byte_count_flag = 1;
+                flag.bytes = 1;
+            else if (strcmp(argv[i], "-l") == 0)
+                flag.lines = 1;
             else {
                 print_usage();
                 return 1;
@@ -44,16 +86,11 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
 
-            int ch;
-            if (byte_count_flag) {
-                while ((ch = fgetc(file)) != EOF)
-                    byte_count++;
+            count_file(file, &count);
+            print_count(count, flag);
 
-                printf("%10d", byte_count);
-                printf("  %-s\n", filename);
-            } else {
-                print_usage();
-            }
+            printf("  %-s\n", filename);
+
 
         } else {
             print_usage();
